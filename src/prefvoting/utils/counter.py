ee63e9a7f5e3bc81c322903a78300dc285f88b8e
Thibaut Durand <durand.tibo@gmail.com>
2025-09-01 17:17:56 -0700

    Add `check_non_empty_count`

--- src/prefvoting/utils/counter.py
+++ src/prefvoting/utils/counter.py
@@ -2,7 +2,7 @@ r"""Contain counter utility functions."""
 
 from __future__ import annotations
 
-__all__ = ["check_non_negative_count"]
+__all__ = ["check_non_empty_count", "check_non_negative_count"]
 
 from typing import TYPE_CHECKING
 
@@ -10,11 +10,38 @@ if TYPE_CHECKING:
     from collections import Counter
 
 
+def check_non_empty_count(counter: Counter) -> None:
+    r"""Check if the counter is not empty.
+
+    Args:
+        counter: The counter to check.
+
+    Raises:
+        ValueError: if the counter is empty.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from collections import Counter
+    >>> from prefvoting.utils.counter import check_non_empty_count
+    >>> check_non_empty_count(Counter({"a": 10, "b": 2, "c": 5, "d": 3}))
+
+    ```
+    """
+    if counter.total() == 0:
+        msg = "The counter is empty"
+        raise ValueError(msg)
+
+
 def check_non_negative_count(counter: Counter) -> None:
     r"""Check if all the count values are non-negative (>=0).
 
     Args:
         counter: The counter to check.
+
+    Raises:
+        ValueError: if at least one count is negative (<0).
 
     Example usage:
 
