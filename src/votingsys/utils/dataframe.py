85be5cf2041580aef03e7cab785602a95de3f05f
Thibaut Durand <durand.tibo@gmail.com>
2025-09-05 01:44:00 -0700

    Fix typo in function name (#20)

--- src/votingsys/utils/dataframe.py
+++ src/votingsys/utils/dataframe.py
@@ -2,7 +2,7 @@ r"""Contain DataFrame utility functions."""
 
 from __future__ import annotations
 
-__all__ = ["check_colum_missing"]
+__all__ = ["check_column_missing"]
 
 from typing import TYPE_CHECKING
 
@@ -10,7 +10,7 @@ if TYPE_CHECKING:
     import polars as pl
 
 
-def check_colum_missing(frame: pl.DataFrame, col: str) -> None:
+def check_column_missing(frame: pl.DataFrame, col: str) -> None:
     r"""Check if a column is missing in a DataFrame.
 
     Args:
@@ -25,8 +25,8 @@ def check_colum_missing(frame: pl.DataFrame, col: str) -> None:
     ```pycon
 
     >>> import polars as pl
-    >>> from votingsys.utils.dataframe import check_colum_missing
-    >>> check_colum_missing(
+    >>> from votingsys.utils.dataframe import check_column_missing
+    >>> check_column_missing(
     ...     pl.DataFrame({"a": [0, 1, 2, 1, 0], "b": [1, 2, 0, 2, 1], "c": [2, 0, 1, 0, 2]}),
     ...     col="col",
     ... )
