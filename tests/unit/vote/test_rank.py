242a9fc5ceaad2535458263f2fa3b1e0f2e0dced
Thibaut Durand <durand.tibo@gmail.com>
2025-09-10 23:26:00 -0700

    Add `RankedVote.from_dataframe` (#47)

--- tests/unit/vote/test_rank.py
+++ tests/unit/vote/test_rank.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import polars as pl
 import pytest
+from coola import objects_are_equal
 
 from votingsys.vote import (
     RankedVote,
@@ -20,6 +21,13 @@ def ranking() -> pl.DataFrame:
 def test_ranked_vote_init_missing_count_col(ranking: pl.DataFrame) -> None:
     with pytest.raises(ValueError, match=r"column 'missing' is missing in the DataFrame:"):
         RankedVote(ranking, count_col="missing")
+
+
+def test_ranked_vote_ranking(ranking: pl.DataFrame) -> None:
+    assert objects_are_equal(
+        RankedVote(ranking).ranking,
+        pl.DataFrame({"a": [0, 1, 2], "b": [1, 2, 0], "c": [2, 0, 1], "count": [3, 5, 2]}),
+    )
 
 
 def test_ranked_vote_repr(ranking: pl.DataFrame) -> None:
@@ -94,3 +102,51 @@ def test_ranked_vote_get_num_voters_2() -> None:
         ).get_num_voters()
         == 11
     )
+
+
+def test_ranked_vote_from_dataframe() -> None:
+    assert RankedVote.from_dataframe(
+        pl.DataFrame(
+            {
+                "a": [0, 0, 0, 1, 1, 1, 1, 1, 2, 2],
+                "b": [1, 1, 1, 2, 2, 2, 2, 2, 0, 0],
+                "c": [2, 2, 2, 0, 0, 0, 0, 0, 1, 1],
+            }
+        )
+    ).equal(
+        RankedVote(
+            pl.DataFrame({"a": [1, 0, 2], "b": [2, 1, 0], "c": [0, 2, 1], "count": [5, 3, 2]})
+        )
+    )
+
+
+def test_ranked_vote_from_dataframe_count_col() -> None:
+    assert RankedVote.from_dataframe(
+        pl.DataFrame(
+            {
+                "a": [0, 0, 0, 1, 1, 1, 1, 1, 2, 2],
+                "b": [1, 1, 1, 2, 2, 2, 2, 2, 0, 0],
+                "c": [2, 2, 2, 0, 0, 0, 0, 0, 1, 1],
+            }
+        ),
+        count_col="#n",
+    ).equal(
+        RankedVote(
+            pl.DataFrame({"a": [1, 0, 2], "b": [2, 1, 0], "c": [0, 2, 1], "#n": [5, 3, 2]}),
+            count_col="#n",
+        )
+    )
+
+
+def test_ranked_vote_from_dataframe_count_col_exist() -> None:
+    with pytest.raises(ValueError, match=r"column 'c' exists in the DataFrame:"):
+        RankedVote.from_dataframe(
+            pl.DataFrame(
+                {
+                    "a": [0, 0, 0, 1, 1, 1, 1, 1, 2, 2],
+                    "b": [1, 1, 1, 2, 2, 2, 2, 2, 0, 0],
+                    "c": [2, 2, 2, 0, 0, 0, 0, 0, 1, 1],
+                }
+            ),
+            count_col="c",
+        )
