be75b2fe44b104233d5904848d7c76f84a3629a2
Thibaut Durand <durand.tibo@gmail.com>
2025-09-15 15:27:55 -0700

    Add workflow to publish package to pypi (debug)

--- src/votingsys/__init__.py
+++ src/votingsys/__init__.py
@@ -1,1 +1,8 @@
 r"""Root package."""
+
+# import importlib.metadata
+#
+# try:
+#     __version__ = importlib.metadata.version(__name__)
+# except importlib.metadata.PackageNotFoundError:
+#     __version__ = "0.0.0-dev"
